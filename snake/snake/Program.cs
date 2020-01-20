343cdebf864cdf682ff0076b80efc881d26bb3e0
artemkudryashov <artem@kudryashov.space>
2020-01-20 18:15:13 +0300

    Written new Method Move for a Snake. Written new Method GetNextPoint for a Snake. Was maked test animation. Written new Method Clear for a Point.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace snake
@@ -25,6 +26,22 @@ namespace snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+            Thread.Sleep( 300 );
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
 
             Console.ReadLine();
             
