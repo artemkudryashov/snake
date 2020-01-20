d02effd8244c5fc80ff8b544d24496292c9fd563
artemkudryashov <artem@kudryashov.space>
2020-01-20 15:15:43 +0300

    Create a class Figure

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,12 +16,16 @@ namespace snake
             Point p2 = new Point(4,5,'#');
             p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(10,30,10,'&');
-            line.Draw();
+            Console.SetBufferSize(80,25);
 
-            VerticalLine vLine = new VerticalLine(5,15,50,'^');
-            vLine.Draw();
-
+            HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
+            HorizontalLine downLine = new HorizontalLine(0,78,24,'+');
+            VerticalLine leftLine = new VerticalLine(0,24,0,'+');
+            VerticalLine rightLine = new VerticalLine(0,24,78,'+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
             Console.ReadLine();
             
         }
