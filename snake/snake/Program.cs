a639337dabfcdfb67d403476b8708f735ce9e217
artemkudryashov <artem@kudryashov.space>
2020-01-26 18:46:30 +0300

    Полиморфизм?! нефига не понятно.ну ок

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,19 +13,25 @@ namespace snake
         {
            Console.SetBufferSize(80,25);
 
-            HorizontalLine upLine = new HorizontalLine(0,78,0,'+');
-            HorizontalLine downLine = new HorizontalLine(0,78,24,'+');
-            VerticalLine leftLine = new VerticalLine(0,24,0,'+');
-            VerticalLine rightLine = new VerticalLine(0,24,78,'+');
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
-
+            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            Draw( v1 );
 
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+            Figure fSnake = new Snake( p, 4, Direction.RIGHT);
+            Draw( fSnake );
+            Snake snake = (Snake) fSnake;
+
+            HorizontalLine h1 = new HorizontalLine(0,5,6,'&');
+
+            List<Figure> figures = new List<Figure>();
+            figures.Add( fSnake );
+            figures.Add( v1 );
+            figures.Add( h1 );
+
+            foreach (var item in figures)
+            {
+                item.Draw();
+            }
 
             FoodCreator foodCreator = new FoodCreator( 80,25,'$' );
             Point food = foodCreator.CreateFood();
@@ -56,6 +62,10 @@ namespace snake
             
         }
 
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
+        }
        
     }
 }
